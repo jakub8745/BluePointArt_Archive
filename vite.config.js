9f5b590d3f896fd5dd3ae451f3afef3de7432537
Blue Poit Art Gallery Project <53195000+jakub8745@users.noreply.github.com>
2024-11-17 20:22:40 +0000

    isdeploy fixed?

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'three-mesh-bvh': path.resolve(__dirname, 'node_modules/three-mesh-bvh/build/index.module.js')
-    }
+  optimizeDeps: {
+    include: ['three', 'three-mesh-bvh'], // Combine the packages into a single array
   },
   assetsInclude: ['**/*.wasm'], // Ensures .wasm files are included in the build
   build: {
-    chunkSizeWarningLimit: 1100, // Set to 1 MB
-    sourcemap: process.env.NODE_ENV === 'development', // Enable source maps only in development
+    rollupOptions: {
+      external: [], // All dependencies will be bundled
+      output: {
+        format: 'es', // Use ES modules in the output
+      },
+    },
+    chunkSizeWarningLimit: 1100, // Set chunk size warning to 1 MB
+    sourcemap: process.env.NODE_ENV === 'development', // Enable source maps in development
   },
 });
