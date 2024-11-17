85eda09d0b7db37b26d2167794c8a75d6623491d
Blue Poit Art Gallery Project <53195000+jakub8745@users.noreply.github.com>
2024-11-17 19:17:16 +0000

    problemy z fleek

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'three-mesh-bvh': path.resolve(__dirname, 'node_modules/three-mesh-bvh')
+    }
+  },
   assetsInclude: ['**/*.wasm'], // Ensures .wasm files are included in the build
   build: {
     chunkSizeWarningLimit: 1100, // Set to 1 MB
-
-    sourcemap: true, // Enable source maps for analysis
+    sourcemap: process.env.NODE_ENV === 'development', // Enable source maps only in development
   },
 });
-
